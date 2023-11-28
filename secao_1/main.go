b95f2e40b8aa56d5a01c6fed5e10590c9602c8cc
Lucas Chies <lchies@gmail.com>
2023-11-28 13:43:00 -0300

    new_updates

---
+++ secao_1/main.go
@@ -0,0 +1,25 @@
+package main
+
+// usando a função import
+//func main() {
+//	if len(os.Args) != 2 {
+//		os.Exit(1)
+//	}
+//	fmt.Println("Hello", os.Args[1])
+
+//fmt.Println("Hello World")
+//}
+
+// Declarando variaveis
+//func main() {
+//	//var name string
+//	//fmt.Println("Hello", name)
+//
+//	name := getName()
+//	idade := 38
+//	fmt.Println("Hello", name, idade)
+//}
+
+//func getName() string {
+//	return "Lucas"
+//}
